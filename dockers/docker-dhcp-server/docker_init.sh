a0f56111e2c023367a823c163a498bc36a3121be
yaqiangz <zyq1512099831@gmail.com>
2023-09-12 09:54:35 +0000

    [dhcp_server] Add dhcpservd support to dhcp_server

--- dockers/docker-dhcp-server/docker_init.sh
+++ dockers/docker-dhcp-server/docker_init.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+
+# Generate supervisord config file
+mkdir -p /etc/supervisor/conf.d/
+# Generate kea folder
+mkdir -p /etc/kea/
+
+# Generate the following files from templates:
+# port-to-alias name map
+CFGGEN_PARAMS=" \
+    -d \
+    -t /usr/share/sonic/templates/port-name-alias-map.txt.j2,/tmp/port-name-alias-map.txt \
+"
+sonic-cfggen $CFGGEN_PARAMS
+
+# Make the script that waits for all interfaces to come up executable
+chmod +x /etc/kea/lease_update.sh
 # The docker container should start this script as PID 1, so now that supervisord is
 # properly configured, we exec /usr/local/bin/supervisord so that it runs as PID 1 for the
 # duration of the container's lifetime
