c62b7dec9f252245a7bf830248165aa4459aafcc
yaqiangz <zyq1512099831@gmail.com>
2023-09-19 15:13:06 +0000

    Add sonic-dhcp-server-cfggen

---
+++ rules/sonic-dhcp-server.mk
@@ -0,0 +1,6 @@
+# sonic-dhcp-server package
+
+SONIC_DHCP_SERVER_PY3 = sonic_dhcp_server-1.0-py3-none-any.whl
+$(SONIC_DHCP_SERVER_PY3)_SRC_PATH = $(SRC_PATH)/sonic-dhcp-server
+$(SONIC_DHCP_SERVER_PY3)_PYTHON_VERSION = 3
+SONIC_PYTHON_WHEELS += $(SONIC_DHCP_SERVER_PY3)
