54bfb5816900e9e7c9367ec12fa825ea5edd5210
yaqiangz <zyq1512099831@gmail.com>
2023-09-19 15:13:06 +0000

    Add sonic-dhcp-server-cfggen

---
+++ src/sonic-dhcp-server/dhcp_server_utils.py
@@ -0,0 +1,92 @@
+from swsscommon import swsscommon
+
+REDIS_SOCK_PATH = "/var/run/redis/redis.sock"
+
+
+class DhcpDbConnector(object):
+    def __init__(self):
+        self.redis_sock = REDIS_SOCK_PATH
+        self.config_db = swsscommon.DBConnector(swsscommon.CONFIG_DB, self.redis_sock, 0)
+        self.state_db = swsscommon.DBConnector(swsscommon.STATE_DB, self.redis_sock, 0)
+
+    def get_config_db_table(self, table_name):
+        """
+        Get table from db.
+        Args:
+            db: An db object.
+            table_name: Name of table want to get.
+        Return:
+            Table objects.
+        """
+        return swsscommon.Table(self.config_db, table_name)
+
+    def get_entry(self, table, entry_name):
+        """
+        Get dict entry from Table object.
+        Args:
+            table: Table object.
+            entry_name: Name of entry.
+        Returns:
+            Dict of entry, sample:
+                {
+                    "customized_options": "option60,option223",
+                    "gateway": "192.168.0.1",
+                    "lease_time": "900",
+                    "mode": "PORT",
+                    "netmask": "255.255.255.0",
+                    "state": "enabled"
+                }
+        """
+        (status, entry) = table.get(entry_name)
+        if not status:
+            return None
+        return dict(entry)
+
+
+def merge_intervals(intervals):
+    """
+    Merge ip range intervals.
+    Args:
+        intervals: Ip ranges, may have overlaps, sample:
+            [
+                [IPv4Address('192.168.0.2'), IPv4Address('192.168.0.5')],
+                [IPv4Address('192.168.0.3'), IPv4Address('192.168.0.6')],
+                [IPv4Address('192.168.0.10'), IPv4Address('192.168.0.10')]
+            ]
+    Returns:
+        Merged ip ranges, sample:
+            [
+                [IPv4Address('192.168.0.2'), IPv4Address('192.168.0.6')],
+                [IPv4Address('192.168.0.10'), IPv4Address('192.168.0.10')]
+            ]
+    """
+    intervals.sort(key=lambda x: x[0])
+    ret = []
+    for interval in intervals:
+        if len(ret) == 0 or interval[0] > ret[-1][-1]:
+            ret.append(interval)
+        else:
+            ret[-1][-1] = max(ret[-1][-1], interval[-1])
+    return ret
+
+
+def get_keys(obj):
+    """
+    Get keys from config db table or db dict
+    Args:
+        intervals: Ip ranges, may have overlaps, sample:
+            [
+                [IPv4Address('192.168.0.2'), IPv4Address('192.168.0.5')],
+                [IPv4Address('192.168.0.3'), IPv4Address('192.168.0.6')],
+                [IPv4Address('192.168.0.10'), IPv4Address('192.168.0.10')]
+            ]
+    Returns:
+        Merged ip ranges, sample:
+            [
+                [IPv4Address('192.168.0.2'), IPv4Address('192.168.0.6')],
+                [IPv4Address('192.168.0.10'), IPv4Address('192.168.0.10')]
+            ]
+    """
+    is_dict = True if isinstance(obj, dict) else False
+    keys = list(obj.keys()) if is_dict else obj.getKeys()
+    return is_dict, keys
---
+++ src/sonic-dhcp-server/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+dependencies = []
+
+test_deps = [
+    "pytest",
+    "mock>=2.0.0",
+    "mockredispy>=2.9.3",
+    "swsssdk>=1.3.0"
+]
+
+py_modules = [
+    "dhcp_server_utils"
+]
+
+setup(
+    name="sonic-dhcp-server",
+    install_requires=dependencies,
+    description="Module of SONiC built-in dhcp_server",
+    version="1.0",
+    url="https://github.com/Azure/sonic-buildimage",
+    tests_require=test_deps,
+    author="SONiC Team",
+    author_email="yaqiangzhu@microsoft.com",
+    setup_requires=[
+        "pytest-runner",
+        "wheel",
+    ],
+    py_modules=py_modules,
+    scripts=[
+        "sonic-dhcp-server-cfggen"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ]
+)
